02213d2110dba420138e01e13de5c26dc6362b02
KalebeFurlan <89493719+KalebeFurlan@users.noreply.github.com>
2023-07-03 17:04:34 -0300

    bla

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <div style="display: flex;" class="mb-2">
-        <h3>BloodyCoin <h3>
+        <h3>BloodyCoin<h3>
         <h3>HOME</h3>
     </div>
     <a href="{{route('login.index')}}">Login</a>
