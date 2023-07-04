314ff8d4ec5aa74afe175bcd26eec8ec99f3a469
KalebeFurlan <89493719+KalebeFurlan@users.noreply.github.com>
2023-07-04 18:10:32 -0300

    AJuste do board home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <div style="display: flex; background-color:red; margin: 0%;" class="mb-2">
-        <h3>BloodyCoin | Home<h3>
+        <h3 class="textin">BloodyCoin | Home<h3>
         <a href="{{route('login.index')}}">Login</a>
     </div>
     <link rel="stylesheet" type="text/css">
@@ -18,3 +18,8 @@
 </body>
 </html>
 @endsection
+<style>
+    .textin{
+        text-decoration-color: white;
+    }
+</style>
