a66e5df9e487a8168ca1423da5c55308e701e1a8
Renan Victor <renan.victor@edu.unifil.br>
2023-07-04 22:11:53 -0300

    Novo commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,21 +5,40 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <div style="display: flex; background-color:red; margin: 0%;" class="mb-2">
-        <h3 class="textin">BloodyCoin | L<h3>
-        <a href="{{route('login.index')}}">ogin</a>
     </div>
     <link rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap" rel="stylesheet" type="text/css">
     <title>HOME</title>
+    <style>
+
+    .textin{
+        color: white;
+        margin-right: 5px;
+        margin-left: 7px;
+    }
+    .board{
+        display: flex; 
+        background-color: red;
+        margin-top: 0px;
+        font-family: 'Noto Sans';
+        src: url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap");
+    }
+
+    
+
+</style>
 </head>
 <body>
-<h3>Sejam Bem-vindos ao BloodyCoins</h3>
+<div class="board">
+        <table border="1" cellpadding="3" cellspacing="0" class="border-color" style="border-color: white">
+    <tr>
+        <td style="width:120px; text-align:center; color:yellow; font-size:40px"><strong>BC</strong></td>
+        <td style="width:120px; color: white; text-align:center"><h3>BloodyCoin</h3></td>
+        <td style="width:1100px; text-align:center; font-size:40px; color:white"><strong>Sejam Bem-vindos ao BloodyCoins</strong></td>
+        <td style="width:150px; text-align:center"><h3><a style="color:white; text-decoration:none" href="{{route('login.index')}}" id="Log">Login</a></h3></td>
+    </tr>
+</table>
+</div>   
 </body>
 </html>
 @endsection
-<style>
-    .textin{
-        text-decoration-color: white;
-    }
-</style>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,8 +1,50 @@
 @extends('master')
 @section('content')
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    </div>
+    <link rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap" rel="stylesheet" type="text/css">
+<style>
+    
+    .container {
+    height: 100vh;
+    background: #ffffff;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    margin-top: 0px;
+    }
+    .box {
+    text-align: center;
+    width: 650px;
+    height: 650px;
+    background: #d9d9d9;
+    }
+    .New{
+        display: flex; 
+        background-color: red;
+        margin-top: 0px;
+        font-family: 'Noto Sans';
+        src: url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300&display=swap");
+    }
+</style>
+
+<div class="New">
 <div style="display: flex; background-color:red; margin: 0%;" class="mb-2">
-        <h3 class="textin">BloodyCoin | H<h3>
-        <a href="{{route('home.index')}}">ome</a>
+<table border="1" cellpadding="3" cellspacing="0" class="border-color" style="border-color: white">
+<tr>
+<td style="width:120px; text-align:center; color:yellow; font-size:40px"><strong>BC</strong></td>
+        <td style="width:120px; color: white; text-align:center"><h3>BloodyCoin</h3></td>
+        <td style="width:1100px; text-align:center; font-size:40px; color:white"><strong>Tela de Login</strong></td>
+        <td style="width:150px; text-align:center"><h3><a style="color:white; text-decoration:none" href="{{route('home.index')}}" id="Hom">Home</a></h3></td>
+    </tr>
+    </table>
+    </div>
     </div>
     @error('error')
         <span>{{$message}}</span>
@@ -22,24 +64,5 @@
                 <button type="submit">Login</button>
             </form>
         </div>
-    </div>
+    </div> 
 @endsection
-<style>
-    .textin{
-        text-decoration-color: white;
-    }
-    .container {
-    width: 100vw;
-    height: 100vh;
-    background: #6C7A89;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    }
-    .box {
-    width: 300px;
-    height: 300px;
-    background: #fff;
-    }
-</style>
